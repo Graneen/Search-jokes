614b79fde77a004566d8bef4559ac3fffbfdc529
Oleg D. <dziekonski@mail.ru>
2024-07-24 17:42:01 +0300

    Context added

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className="central_block">
+        <section className="Container_container__UCC1o Container_md__q_A3U">
+          <div className="SearchJokesWizard_searchJokes__oQ9wI">
+            <input className="SearchJokesWizard_searchJokesInput__xfVc_" type="text" placeholder="Search jokes..." value=""/>
+          </div>
+          <ul className="JokesList_jokesList__u89Dn">
+            <span>No jokes here</span></ul>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
